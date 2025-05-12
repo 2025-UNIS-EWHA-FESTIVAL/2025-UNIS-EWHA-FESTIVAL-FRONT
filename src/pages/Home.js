43a98597a2d08ed90878e557b1919fb96d9f21c1
eunkr82 <eun.kr82@gmail.com>
2025-05-12 15:17:39 +0900

    fix: Home.js 사파리 브라우저 font 여유공간 확보, height 수정정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import ReviewCarousel from "../components/ReviewCarousel";
 const home = () => {
     return (
         <div className="flex flex-col justify-center items-center w-screen h-screen overflow-hidden">
-            <div className="relative w-[375px] h-full mx-auto bg-cover bg-center overflow-hidden"
+            <div className="relative w-[375px] h-[667px] mx-auto bg-cover bg-center overflow-hidden"
             style={{ backgroundImage: "url('/assets/home.svg')" }}>
                     <div className="absolute top-0 left-0 w-full h-[85px]">
                         <AdBanner/>
@@ -21,7 +21,7 @@ const home = () => {
 
                     <div className="absolute w-full h-[184px] pr-[12px] pl-[12px] mb-[30px] bottom-0">
                         <div className="w-[155px] h-[24px] font-pretendard bg-[#00FF15] text-center text-[14px] font-black"> ◆ 벗들의 진실된 후기 ◆</div>
-                        <div className="w-[247px] h-[18px] font-pretendard bg-[#00FF15] text-[#ff0000] mt-[7px] font-black text-center text-[10px] border border-black">가짜? 없습니다! 200% 진실된 후기! 여러분도 가능합니다</div>
+                        <div className="w-[250px] h-[18px] font-pretendard bg-[#00FF15] text-[#ff0000] mt-[7px] font-black text-center text-[10px] border border-black">가짜? 없습니다! 200% 진실된 후기! 여러분도 가능합니다</div>
                         <div className="w-full h-[125px] mt-[10px]">
                             <ReviewCarousel />
                         </div>
