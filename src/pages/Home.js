659fd4dc6bc72a0de77fd6914fc573f81e532130
eunkr82 <eun.kr82@gmail.com>
2025-05-12 15:13:44 +0900

    fix: 사파리 브라우저 폰트 깨짐 수정 시도

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,8 +20,8 @@ const home = () => {
                     </Link>
 
                     <div className="absolute w-full h-[184px] pr-[12px] pl-[12px] mb-[30px] bottom-0">
-                        <div className="w-[150px] h-[24px] font-pretendard bg-[#00FF15] text-center text-[14px] font-black"> ◆ 벗들의 진실된 후기 ◆</div>
-                        <div className="w-[241px] h-[18px] font-pretendard bg-[#00FF15] text-[#ff0000] mt-[7px] font-black text-center text-[10px] border border-black">가짜? 없습니다! 200% 진실된 후기! 여러분도 가능합니다</div>
+                        <div className="w-[155px] h-[24px] font-pretendard bg-[#00FF15] text-center text-[14px] font-black"> ◆ 벗들의 진실된 후기 ◆</div>
+                        <div className="w-[247px] h-[18px] font-pretendard bg-[#00FF15] text-[#ff0000] mt-[7px] font-black text-center text-[10px] border border-black">가짜? 없습니다! 200% 진실된 후기! 여러분도 가능합니다</div>
                         <div className="w-full h-[125px] mt-[10px]">
                             <ReviewCarousel />
                         </div>
